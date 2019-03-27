f354ec9c99a000c1fd33c50736d158ed661e7ea9
Michael <wisdominlyrics@gmail.com>
2019-03-27 14:07:10 -0400

    added tests

--- bracketbuilder/test/best_test.go
+++ bracketbuilder/test/best_test.go
@@ -6,8 +6,20 @@ import (
 	"testing"
 )
 
-func TestHTML(t *testing.T) {
-	test, err := functions.GetHTML("this is not a url")
-	fmt.Println(test, err)
-	err == error("Get this%20is%20not%20a%20url: unsupported protocol scheme")
+// Test that an error is returned when a bad url is given
+func TestBadHTML(t *testing.T) {
+	_, err := functions.GetHTML("this is not a url")
+	if err != nil {
+		fmt.Println("properly detected invalid url", err)
+	}
+}
+
+// Test that checks if an error is returned when a good url is given
+func TestGoodHtml(t *testing.T) {
+	_, err := functions.GetHTML("https://www.example.com")
+	if err == nil {
+
+	} else {
+		t.Error("Unexpected Error, check connectivity and try again")
+	}
 }
